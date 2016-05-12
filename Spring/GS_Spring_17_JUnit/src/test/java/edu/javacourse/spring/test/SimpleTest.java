777e02c8bc06f1299710d361c60e509960851920
Lyolya <lelagorizont@yandex.ru>
2016-05-12 09:02:31 +0400

    спринг для тестирования

--- Spring/GS_Spring_17_JUnit/src/test/java/edu/javacourse/spring/test/SimpleTest.java
+++ Spring/GS_Spring_17_JUnit/src/test/java/edu/javacourse/spring/test/SimpleTest.java
@@ -1,21 +1,24 @@
 package edu.javacourse.spring.test;
 
 import edu.javacourse.spring.bean.RegionManager;
-import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertNotNull;
+
 /**
- * Author: Georgy Gobozov
- * Date: 18.07.13
+ * RunWith аннот junit. Вставляется класс, который junit будет использовать
+ * У спринга есть спец класс, зависимость spring-test
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:springExample.xml"})
-public class SimpleTest extends TestCase {
-
+public class SimpleTest {
+    @Autowired
+    ApplicationContext context;
 
     @Autowired
     RegionManager manager;

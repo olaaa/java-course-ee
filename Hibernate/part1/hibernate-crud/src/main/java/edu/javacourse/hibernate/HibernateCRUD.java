7bf63571908f8a320e0edb97ac2e8e804f7709f3
Lyolya <lelagorizont@yandex.ru>
2016-05-15 11:56:58 +0400

    CRUD load vs get

--- Hibernate/part1/hibernate-crud/src/main/java/edu/javacourse/hibernate/HibernateCRUD.java
+++ Hibernate/part1/hibernate-crud/src/main/java/edu/javacourse/hibernate/HibernateCRUD.java
@@ -1,5 +1,7 @@
 package edu.javacourse.hibernate;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -16,10 +18,14 @@ import java.io.Serializable;
  */
 public class HibernateCRUD {
 
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+
     private static final Logger log = LoggerFactory.getLogger(HibernateCRUD.class);
 
     private static SessionFactory sessionFactory;
     private static ServiceRegistry serviceRegistry;
+    private static Serializable firstCityId;
+    private static Serializable secondCityId;
 
     private static void init() {
         Configuration configuration = new Configuration();
@@ -78,7 +84,11 @@ public class HibernateCRUD {
         session.getTransaction().commit();
     }
 
+/*Based on the above explanations we have following differences between get() vs load():
 
+get() loads the data as soon as it’s called whereas load() returns a proxy object and loads data only when it’s actually required, so load() is better because it support lazy loading.
+Since load() throws exception when data is not found, we should use it only when we know data exists.
+We should use get() when we want to make sure data exists in the database.*/
     private static void getVsLoad() {
         log.info("==============GET_VS_LOAD=================");
         Session session = sessionFactory.getCurrentSession();
@@ -87,27 +97,34 @@ public class HibernateCRUD {
         City city1 = new City();
         city1.setCityName("Surgut");
         city1.setRegion(region);
-        session.save(city1);
-
-        region = (Region) session.load(Region.class, id);
+        firstCityId = session.save(city1); // выполнился инсерт
+//        insert into public.jc_city(city_name, region_id) values( ?,?)
+        region = (Region) session.load(Region.class, id); // select не выполнился
         City city2 = new City();
         city2.setCityName("Nizhnevartovsk");
         city2.setRegion(region);
-        id = session.save(city2);
+        secondCityId = session.save(city2); // выполнился инсерт
+//        insert into public.jc_city(city_name, region_id) values( ?,?)
+        region = (Region) session.get(Region.class, id); // select не выполнился
 
-        session.getTransaction().commit();
+        session.getTransaction().commit(); // отобразились изменения в базе
     }
 
     private static void update() {
         log.info("==============UPDATE=================");
         Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
-        City city = (City) session.load(City.class, id);
+        // load — будет прокси-тип, get — просто City
+        City city = (City) session.get(City.class, firstCityId); // здесь будет прокси-тип
         city.setCityName("nefteugansk");
-        session.saveOrUpdate(city);
+        session.saveOrUpdate(city); // insert не выполнгился
         log.info("city = {}", city);
 
-        session.getTransaction().commit();
+        session.getTransaction().commit();// выполнился update
+//        update public.jc_city set city_name=?, region_id=? where city_id=?
+//        binding parameter[ 1]as[VARCHAR] -[nefteugansk]
+//        binding parameter[ 2]as[BIGINT] -[32]
+//        binding parameter[ 3]as[INTEGER] -[41]
     }
 
 
@@ -116,10 +133,11 @@ public class HibernateCRUD {
         Session session = sessionFactory.getCurrentSession();
         session.beginTransaction();
         City city = (City) session.load(City.class, id);
-
         session.delete(city);
 
-        session.getTransaction().commit();
-    }
+        City city2 = (City) session.load(City.class, secondCityId);
+        session.delete(city2);
 
+        session.getTransaction().commit(); // выполнились оба делита
+    }
 }
